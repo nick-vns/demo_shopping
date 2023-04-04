8844edb1e58d329c78f69d88441931544f185b61
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-04-04 14:07:51 -0600

    demo app with firesbase db

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import ShoppingLists from "./components/ShoppingLists";
+import Welcome from "./components/Welcome";
+
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCjsL1Lj3-q8Bea1MqigEgpZEYAN-0PLGM",
+    authDomain: "shopping-list-demo-13e1e.firebaseapp.com",
+    projectId: "shopping-list-demo-13e1e",
+    storageBucket: "shopping-list-demo-13e1e.appspot.com",
+    messagingSenderId: "901033793870",
+    appId: "1:901033793870:web:6043e7452cdfcd77c1661f",
+    measurementId: "G-MCHLCTWYMK",
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="ShoppingLists">
+          {(props) => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
